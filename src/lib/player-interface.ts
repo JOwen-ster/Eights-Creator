5dc6d8dee026edeb7ca32bc0fc5569c5a6544e2f
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-04-16 14:47:57 -0700

    update

---
+++ src/lib/player-interface.ts
@@ -0,0 +1,16 @@
+interface Players {
+    backlines: {
+        player_1: '',
+        player_2: ''
+    },
+    supports: {
+        player_1: '',
+        player_2: ''
+    },
+    slayers: {
+        player_1: '',
+        player_2: '',
+        player_3: '',
+        player_4: ''
+    }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
+<script lang="ts">
+    let all_players: Players = {
+        backlines : {
+            player_1: '',
+            player_2: '',
+        },
+        supports : {
+            player_1: '',
+            player_2: '',
+        },
+        slayers : {
+            player_1: '',
+            player_2: '',
+            player_3: '',
+            player_4: '',
+        }
+    };
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
